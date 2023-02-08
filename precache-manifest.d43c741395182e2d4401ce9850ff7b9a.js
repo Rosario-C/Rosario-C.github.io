4a009508e2073ed92f89b547631d62b791d826a8
Rosario-C <rosarioceballoscarril@hotmail.com>
2023-02-08 12:26:10 -0300

    Updates

---
+++ precache-manifest.d43c741395182e2d4401ce9850ff7b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9146448b7114240e604",
+    "url": "/Rosario-C/Rosario-C.github.io.git/static/css/main.5010d39b.chunk.css"
+  },
+  {
+    "revision": "c9146448b7114240e604",
+    "url": "/Rosario-C/Rosario-C.github.io.git/static/js/main.c9146448.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/Rosario-C/Rosario-C.github.io.git/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "d3d62ae5d49296b63896",
+    "url": "/Rosario-C/Rosario-C.github.io.git/static/js/2.d3d62ae5.chunk.js"
+  },
+  {
+    "revision": "5cbc84520c4b1dde3f5d",
+    "url": "/Rosario-C/Rosario-C.github.io.git/static/js/runtime~main.5cbc8452.js"
+  },
+  {
+    "revision": "cb3ccfb134eeb583cab610518797dcb7",
+    "url": "/Rosario-C/Rosario-C.github.io.git/static/media/FreeCodeCamp_logo.svg.cb3ccfb1.png"
+  },
+  {
+    "revision": "9b5fa8772b41ddd5448928d342240852",
+    "url": "/Rosario-C/Rosario-C.github.io.git/index.html"
+  }
+];
